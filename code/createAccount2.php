bba685ae303fd7068f7d6a2d6b6f29bb40276dca
Javios <plmgzm@gmail.com>
2014-08-05 17:03:20 -0400

    Create test polymer createAccount page

--- code/createAccount2.php
+++ code/createAccount2.php
@@ -5,101 +5,101 @@
 
 <?php
 
-//include("config.php");
-//session_start();
-//
-//if ($_SERVER["REQUEST_METHOD"] == "POST")
-//{
-//   if (empty($_POST["firstname"]))
-//     {$nameErr = "First Name is required";}
-//   else
-//     {
-//     $firstName = test_input($_POST["firstname"]);
-//     // check if name only contains letters and whitespace
-//     if (!preg_match("/^[a-zA-Z ]*$/",$firstname))
-//       {
-//       $nameErr = "Only letters and white space allowed"; 
-//       }
-//     }
-//   
-//      if (empty($_POST["lastname"]))
-//     {$nameErr = "Last Name is required";}
-//   else
-//     {
-//     $lastName = test_input($_POST["lastname"]);
-//     // check if name only contains letters and whitespace
-//     if (!preg_match("/^[a-zA-Z ]*$/",$lastname))
-//       {
-//       $nameErr = "Only letters and white space allowed"; 
-//       }
-//     }
-//     
-//   if (empty($_POST["school"]))
-//     {$schoolErr = "School is required";}
-//   else
-//     {
-//     $school = test_input($_POST["school"]);
-//     // check if name only contains letters and whitespace
-//     if (!preg_match("/^[a-zA-Z ]*$/",$school))
-//       {
-//       $schoolErr = "Only letters and white space allowed for School Name";
-//       }
-//     }
-//   
-//   if (empty($_POST["email"]))
-//     {$emailErr = "Email is required";}
-//   else
-//     {
-//     $email = test_input($_POST["email"]);
-//     // check if e-mail address syntax is valid
-//     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
-//       {
-//       $emailErr = "Invalid email format"; 
-//       }
-//     }
-//     
-//   if (empty($_POST["password"]))
-//     {$passwordErr = "Password is required";}
-//   else
-//     {
-//     $password = test_input($_POST["password"]);
-//     // check if password is valid
-//     if (!preg_match("/^[a-zA-Z ]*$/",$password))
-//       {
-//       $passwordErr = "Invalid password format"; 
-//       }
-//     }
-//     
-//   if (empty($_POST["website"]))
-//     {$website = "";}
-//   else
-//     {
-//     $website = test_input($_POST["website"]);
-//     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-//     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website))
-//       {
-//       $websiteErr = "Invalid URL"; 
-//       }
-//     }
-//
-//   if (empty($_POST["comment"]))
-//     {$comment = "";}
-//   else
-//     {$comment = test_input($_POST["comment"]);}
-//
-//   if (empty($_POST["clickedGender"]))
-//     {$genderErr = "Gender is required";}
-//   else
-//     {$gender = test_input($_POST["clickedGender"]);}
-//}
-//
-//function test_input($data)
-//{
-//     $data = trim($data);
-//     $data = stripslashes($data);
-//     $data = htmlspecialchars($data);
-//     return $data;
-//}
+include("config.php");
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  if (empty($_POST["firstname"]))
+    {$nameErr = "First Name is required";}
+  else
+    {
+    $firstName = test_input($_POST["firstname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$firstname))
+      {
+      $nameErr = "Only letters and white space allowed"; 
+      }
+    }
+  
+     if (empty($_POST["lastname"]))
+    {$nameErr = "Last Name is required";}
+  else
+    {
+    $lastName = test_input($_POST["lastname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$lastname))
+      {
+      $nameErr = "Only letters and white space allowed"; 
+      }
+    }
+    
+  if (empty($_POST["school"]))
+    {$schoolErr = "School is required";}
+  else
+    {
+    $school = test_input($_POST["school"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$school))
+      {
+      $schoolErr = "Only letters and white space allowed for School Name";
+      }
+    }
+  
+  if (empty($_POST["email"]))
+    {$emailErr = "Email is required";}
+  else
+    {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address syntax is valid
+    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+      {
+      $emailErr = "Invalid email format"; 
+      }
+    }
+    
+  if (empty($_POST["password"]))
+    {$passwordErr = "Password is required";}
+  else
+    {
+    $password = test_input($_POST["password"]);
+    // check if password is valid
+    if (!preg_match("/^[a-zA-Z ]*$/",$password))
+      {
+      $passwordErr = "Invalid password format"; 
+      }
+    }
+    
+  if (empty($_POST["website"]))
+    {$website = "";}
+  else
+    {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website))
+      {
+      $websiteErr = "Invalid URL"; 
+      }
+    }
+
+  if (empty($_POST["comment"]))
+    {$comment = "";}
+  else
+    {$comment = test_input($_POST["comment"]);}
+
+  if (empty($_POST["clickedGender"]))
+    {$genderErr = "Gender is required";}
+  else
+    {$gender = test_input($_POST["clickedGender"]);}
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 $districtId = 1;
 
