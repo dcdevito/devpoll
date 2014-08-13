01c2050e40e3aa0534155a8fea864fc304b80ed0
Javios <plmgzm@gmail.com>
2014-08-13 17:18:29 -0400

    Checking in latest

---
+++ playground/editselectedsurvey.php
@@ -0,0 +1,258 @@
+<?php
+	// Make sure the person is logged in.
+	include("verifylogin.php");
+?>
+
+<html>
+	<head>
+		<script>
+			function includeQuestions()
+			{
+				alert("Include questions in survey");
+
+				// Launch the Include Questions page.
+				window.location = "includequestions.php";
+			}
+		</script>
+	</head>
+	<body>
+<?php
+
+		echo "We are in editselectedsurvey<br/>";
+
+		//if(!empty($_POST['editSurveyQuestion']))
+		//{	
+			$surveyId = $_POST['editsurvey'];
+
+			echo "We will be editing survey $surveyId<br/>";
+		//}
+?>
+
+<?php
+	/*
+		Load all of the questions from the database into a grid (i.e. a table).
+		Next to each row there will be an edit and delete button.
+		The edit button will allow the person to change the question and the answers.
+		The delete button will remove the question from the survey.
+	*/
+	echo "We are in editselectedsurvey.php<br/>";
+	echo "SurveyId = $surveyId<br/>";
+	
+	session_start();
+	$_SESSION['surveyId'] = $surveyId;
+
+	$result = getDistrictQuestionsAndAnsers(1, $surveyId);
+
+	echo "The number of rows we got is ".$result->num_rows;
+
+	echo "About to call displayEditQuestionsAndAnswers<br/>";
+
+	displayEditQuestionsAndAnswers($result, $surveyId);
+
+	echo "Called it<br/>";
+
+	function getDistrictQuestionsAndAnsers($districtId, $surveyId)
+	{
+		// Connect to the database.
+		require("connectToDB.php");
+
+		echo "In display questions and districtId = $districtId<br/>";
+		$questionQuery = "
+						SELECT
+						q.questionid,
+						s.surveyname,
+						q.questionnumber,
+						q.questiontext,
+						q.questiontype,
+						a.answernumber,
+						a.answertext,
+						a.lowvalue,
+						a.highvalue,
+						a.lowdescription,
+						a.highdescription
+						FROM devpoll.questions q
+						JOIN devpoll.answers a
+						ON q.surveyid = a.surveyid
+						AND q.questionnumber = a.questionnumber
+						JOIN devpoll.survey s
+						ON q.surveyid = s.surveyid
+						WHERE s.districtId = $districtId
+						AND s.surveyid = $surveyId;
+		";
+
+		// Get the questions and answers for this survey.
+		$result = $conn->query($questionQuery);
+
+		// Close the connection.
+		$conn->close();		
+
+		return $result;
+	}
+
+
+	function displayEditQuestionsAndAnswers($result, $surveyId)
+	{
+		echo "<form action='saveeditsurvey.php' method='POST'>";
+		echo "<table border='1' cellspacing='0' cellpadding='0' width='80%' style='border-color:LightGrey'>";
+		echo "<tr>";
+		echo "<th>select</th>";
+		echo "<th>Survey Name</th>";
+		echo "<th>Question Number</th>";
+		echo "<th>Question</th>";
+		echo "<th>Question Type</th>";
+		echo "<th colspan='5'>Answers</th>";
+		echo "</tr>";
+
+		// Initialize the value of $questionNumber.
+		$questionNumber = -1;
+		$loop = 0;
+		$backcolorflag = 0;
+		$backcolor = "WhiteSmoke";
+		$answercount = 0;
+
+		while($row = $result->fetch_assoc()) 
+		{
+			$surveyName = $row['surveyname'];
+			$questionText = $row['questiontext'];
+			$questionType = $row['questiontype'];
+			$answerNumber = $row['answernumber'] + 1;
+			$answerText = $row['answertext'];
+
+			$qn = $row['questionnumber'];
+			
+			if ($questionNumber != $qn)
+			{
+				if ($backcolorflag == 0)
+				{
+					$backcolor = "white";
+					$backcolorflag = 1;
+				}
+				else
+				{
+					$backcolor = "WhiteSmoke";
+					$backcolorflag = 0;
+				}	
+
+				if ($answercount > 0 && $answercount < 5)
+				{
+					$colspanAmount = 5 - $answercount;
+
+					echo "<td colspan='$colspanAmount'>$nbsp</td>";
+					echo "</tr>";
+				}
+				$answercount = 0;
+
+				$questionNumber = $qn;
+				$questionid = $row['questionid'];
+
+				echo "<tr bgcolor='$backcolor'>";
+				echo "<td>
+						<input type='checkbox' name='includeSurveyQuestion[]' value='$questionid'>
+						<input type='hidden' name='surveyId' value='$surveyId'>
+						</td>";
+				echo "<td>$surveyName</td>";
+				echo "<td>$questionNumber</td>";
+				echo "<td>$questionText</td>";
+
+				switch($questionType)
+				{
+					case "freeForm":
+						echo "<td>Free Form Text</td>";
+						echo "<td colspan='5'>&nbsp;</td>";
+						echo "</tr>";
+						break;
+					case "rating":
+						echo "<td>Rating</td>";
+
+						$lowDescription = $row['lowdescription'];
+						$highDescription = $row['highdescription'];
+						$lowValue = $row['lowvalue'];
+						$highValue = $row['highvalue'];
+
+						echo "<td colspan='2'>$lowDescription</td>";
+						echo "<td colspan='2'>$highDescription</td>";
+						echo "<td>&nbsp;</td>";
+						echo "</tr>";
+						echo "<tr bgcolor='$backcolor'>";
+						echo "<td colspan='5'>&nbsp;</td>";
+						echo "<td colspan='2'>$lowValue</td>";
+						echo "<td colspan='2'>$highValue</td>";
+						echo "<td>&nbsp;</td>";
+						echo "</tr>";
+
+						break;
+					case "trueFalse":
+						echo "<td>True False</td>";
+						echo "<td colspan='2'>$answerText</td>";
+						break;
+					case "multipleChoice":
+						$loop = 0;
+
+						echo "<td>Multiple Choice</td>";
+						echo "<td>$answerText</td>";
+						break;
+					case "severalAnswer":
+						$loop = 0;
+
+						echo "<td>Several Answer</td>";
+						echo "<td>$answerText</td>";
+						break;
+				}
+			}
+			else
+			{
+				switch($questionType)
+				{
+					case "freeForm":
+						$answercount = 5;
+						break;
+					case "rating":
+						$answercount = 5;
+						break;
+					case "trueFalse":
+						echo "<td colspan='3'>$answerText</td>";
+						echo "</tr>";
+						$answercount = 5;
+						break;
+					case "multipleChoice":
+						if ($loop == 5)
+						{
+							echo "</tr>";
+							echo "<tr bgcolor='$backcolor'>";
+							echo "<td colspan='5'>&nbsp;</td>";
+
+							$answercount = 0;
+						}
+
+						echo "<td>$answerText</td>";
+						$answercount++;
+						break;
+					case "severalAnswer":
+						if ($loop == 5)
+						{
+							echo "</tr>";
+							echo "<tr bgcolor='$backcolor'>";
+							echo "<td colspan='5'>&nbsp;</td>";
+
+							$answercount = 0;
+						}
+
+						echo "<td>$answerText</td>";
+						$answercount++;
+						break;
+				}			
+			}
+
+			$loop++;
+		}
+
+//		echo "</tr>";
+		echo "</table>";
+		echo "<input type='submit' value='Edit Question'>";
+		echo "<input type='button' onclick='includeQuestions();'>";
+		echo "</form>";
+	}
+?>
+	</body>
+</html>
+
---
+++ playground/includequestionsinsurvey.php
@@ -0,0 +1,202 @@
+<?php
+	echo "Inside includequestionsinsurvey.php<br/>";
+
+	try
+	{
+		$surveyId = $_POST['surveyId'];
+
+		//echo "The surveyId is $surveyId<br/>";
+
+		if(!empty($_POST['includeSurveyQuestion']))
+		{		
+			// Get the maximum question number for this survey.
+			$questionNumber = getMaxQuestionNumber($surveyId);
+
+			//echo "The max question was $questionNumber<br/>";
+
+			foreach($_POST['includeSurveyQuestion'] as $questionId)
+			{
+				//echo "Question Id = $questionId<br/>";
+				$questionResult = getQuestionForId($questionId);
+				$answerResult = getAnswersForId($questionId);
+
+				//echo "Number of questions = ".$questionResult->num_rows;
+				//echo "Number of answers = ".$answerResult->num_rows;
+
+				insertQuestionToSurvey($surveyId, ++$questionNumber, $questionResult, $answerResult);
+			}
+		}
+
+		header('Location: createsurvey.php');	
+	}
+	catch(Exception $e)
+	{
+		echo "Error in includequestionsinsurvey ", $e->getMessage(), "<br/>";
+	}
+
+	function getMaxQuestionNumber($surveyId)
+	{
+		// Connect to the database.
+		require("connectToDB.php");
+
+		$numberQuery = "
+						SELECT max(q.questionnumber) as questionnumber
+						FROM devpoll.questions
+						WHERE surveyid = $surveyId;
+						";
+
+		// Get the questions and answers for this survey.
+		$max = $conn->query($questionQuery);
+
+		// If there are no questions - set max question to zero.
+		if ($max == null)
+		{
+			$max = 0;
+		}
+
+		// Close the connection.
+		$conn->close();
+
+		// Return the result.
+		return $max;		
+
+	}
+
+	function getQuestionForId($questionId)
+	{
+		// Connect to the database.
+		require("connectToDB.php");
+
+		//echo "In getQuestionForId = $questionId<br/>";
+		
+		$questionQuery = "
+						SELECT
+						q.surveyid,
+						q.questionid,
+						q.questionnumber,
+						q.questiontext,
+						q.questiontype,
+						q.datecreated
+						FROM devpoll.questions q
+						WHERE q.questionid = $questionId;
+		";
+
+		//echo "Question query = $questionQuery<br/>";
+
+		// Get the questions and answers for this survey.
+		$result = $conn->query($questionQuery);
+
+		//echo $conn->errno." ".$conn->error;
+
+		// Close the connection.
+		$conn->close();
+
+		// Return the result.
+		return $result;		
+	}
+
+	function getAnswersForId($questionId)
+	{
+		// Connect to the database.
+		require("connectToDB.php");
+
+		echo "In getAnswersForId = $questionId<br/>";
+		
+		$questionQuery = "
+						SELECT
+						a.surveyid,
+						a.answernumber,
+						a.answertext,
+						coalesce(a.lowvalue, 0) as lowvalue,
+						coalesce(a.highvalue, 0) as highvalue,
+						a.lowdescription,
+						a.highdescription,
+						a.datecreated
+						FROM devpoll.questions q
+						JOIN devpoll.answers a
+						ON q.surveyid = a.surveyid
+						AND q.questionnumber = a.questionnumber
+						WHERE q.questionid = $questionId;
+		";
+
+		// Get the questions and answers for this survey.
+		$result = $conn->query($questionQuery);
+
+		// Close the connection.
+		$conn->close();
+
+		// Return the result.
+		return $result;		
+	}
+
+	function insertQuestionToSurvey($surveyId, $questionNumber, $questionResult, $answerResult)
+	{
+		echo "Question number = $questionNumber<br/>";
+
+		// Connect to the database.
+		require("connectToDB.php");
+
+		// Start the Transaction.
+    	$conn->autocommit(FALSE);
+
+		try 
+		{
+	    	echo "We are inserting the question<br/>";
+
+	    	while ($row = $questionResult->fetch_assoc())
+	    	{
+	    		$questionText = $row['questiontext'];
+	    		$questionType = $row['questiontype'];
+	    		$dateCreated = $row['datecreated'];
+
+	    		$sql = "INSERT INTO devpoll.questions(surveyid, questionnumber, questiontext, questiontype, datecreated, lastmodified) 
+	    				VALUES($surveyId, $questionNumber, '$questionText', '$questionType', '$dateCreated', 'now()');";
+
+	    		echo "sql = $sql<br/>";
+
+	    		$conn->query($sql);
+
+	    		echo "Insert done: ".$conn->errno." ".$conn->error."<br/>";
+	    	}
+
+	    	echo "We are inserting the answer<br/>";
+
+	    	while ($row = $answerResult->fetch_assoc())
+	    	{
+	    		$answerNumber = $row['answernumber'];
+	    		$answerText = $row['answertext'];
+	    		$lowValue = $row['lowvalue'];
+	    		$highValue = $row['highvalue'];
+	    		$lowDescription = $row['lowdescription'];
+	    		$highDescription = $row['highdescription'];
+	    		$dateCreated = $row['datecreated'];
+
+	    		$sql = "INSERT INTO devpoll.answers(surveyid, questionnumber, answernumber, answertext, 
+	    											lowvalue, highvalue, lowdescription, highdescription, datecreated) 
+	    				VALUES($surveyId, $questionNumber, $answerNumber, '$answerText', 
+	    						$lowValue, $highValue, '$lowDescription', '$highDescription', '$dateCreated');";
+
+	    		echo "sql = $sql<br/>";
+
+	    		$conn->query($sql);
+
+	    		echo "Insert done: ".$conn->errno." ".$conn->error."<br/>";   		
+	    	}
+	
+			// Commit the SQL queries and go back to non-transaction mode.
+    		$conn->commit();
+		}
+		catch ( Exception $e ) 
+		{
+    		// before rolling back the transaction, you'd want
+    		// to make sure that the exception was db-related
+    		$conn->rollback(); 
+		}	
+
+		// End the transaction.    	
+    	$conn->autocommit(TRUE); // i.e., end transaction   
+
+    	// Close the connection.
+    	$conn->close();
+    }
+?>
